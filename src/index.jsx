e893f6cb93aa4d0c368b49045667e9f414739fa6
Ammar M. Kamel <ammarkamel9899@gmail.com>
2023-09-27 12:05:40 +0300

    [Feature] Add Material UI

--- src/index.jsx
+++ src/index.jsx
@@ -5,6 +5,11 @@ import store from './store/store';
 
 import './styles/index.css';
 
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 import App from "./App";
 
 
