8d1d4e6727aeddbe9bb6be0a01d3e35886f73034
Ammar M. Kamel <ammarkamel9899@gmail.com>
2023-09-28 21:00:35 +0300

    [Feature] Use API to display movies dynamically

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { getImageUrl } from "../../api/services/imageServices";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
@@ -7,66 +8,78 @@ import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
 import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
 import CircularProgressWithLabel from "./CircularProgressWithLabel";
 
-export default function MovieCard() {
-  return (
-    <Card
-      sx={{
-        maxWidth: 345,
-        position: "relative",
-        boxShadow: "none",
-        borderRadius: "10px",
-        backgroundColor: "var(--bg-color)",
-        "@media (max-width:600px)": {
-          width: "auto",
-          margin: "0 auto",
-        },
-      }}
-    >
-      <IconButton
-        sx={{
-          position: "absolute",
-          top: "2%",
-          right: "5%",
-          backgroundColor: "#fff",
-          opacity: "0.5",
-          ":hover": {
-            backgroundColor: "#ddd",
-            opacity: "1",
-          },
-        }}
-      >
-        <MoreHorizIcon sx={{ fontSize: 14 }} />
-      </IconButton>
-      <figure>
-        <img
-          src="https://image.tmdb.org/t/p/w500/1qCq228LsNtUseCnNE7Naw6NBUz.jpg"
-          style={{ width: "100%", borderRadius: "10px" }}
-        />
-      </figure>
-      <CardContent sx={{ position: "relative" }}>
-        <CircularProgressWithLabel value={30} />
-        <Typography
-          gutterBottom
-          variant="h6"
-          component="h6"
-          color= "var(--text-color)"
-          sx={{ fontSize: "0.874rem", fontWeight: "bold", mt: 2 }}
+export default function MovieCard(props) {
+    return (
+        <Card
+            sx={{
+                maxWidth: 345,
+                position: "relative",
+                boxShadow: "none",
+                borderRadius: "10px",
+                backgroundColor: "var(--bg-color)",
+                "@media (max-width:600px)": {
+                    width: "auto",
+                    margin: "0 auto",
+                },
+            }}
         >
-          La Science des rêves
-        </Typography>
-        <Typography
-          variant="body2"
-          color= "var(--text-color)"
-          sx={{
-            display: "flex",
-            justifyContent: "space-between",
-            alignItems: "center",
-          }}
-        >
-          Sep 25, 2017
-          <FavoriteBorderOutlinedIcon sx={{ color: "var(--primary-color)" }} />
-        </Typography>
-      </CardContent>
-    </Card>
-  );
+            <IconButton
+                sx={{
+                    position: "absolute",
+                    top: "2%",
+                    right: "5%",
+                    backgroundColor: "#fff",
+                    opacity: "0.5",
+                    ":hover": {
+                        backgroundColor: "#ddd",
+                        opacity: "1",
+                    },
+                }}
+            >
+                <MoreHorizIcon sx={{ fontSize: 14 }} />
+            </IconButton>
+            <figure>
+                <img
+                    src={getImageUrl(props.movie.poster_path)}
+                    style={{ width: "100%", borderRadius: "10px" }}
+                    alt={props.movie.title}
+                />
+            </figure>
+            <CardContent sx={{ position: "relative" }}>
+                <CircularProgressWithLabel
+                    value={props.movie.vote_average * 10}
+                />
+                <Typography
+                    gutterBottom
+                    variant="h6"
+                    component="h6"
+                    color="var(--text-color)"
+                    sx={{ fontSize: "0.874rem", fontWeight: "bold", mt: 2 }}
+                >
+                    {props.movie.original_title}
+                </Typography>
+                <Typography
+                    variant="body2"
+                    color="var(--text-color)"
+                    sx={{
+                        display: "flex",
+                        justifyContent: "space-between",
+                        alignItems: "center",
+                    }}
+                >
+                    {new Date(props.movie.release_date).toLocaleDateString(
+                        "en-US",
+                        {
+                            month: "short",
+                            day: "numeric",
+                            year: "numeric",
+                        }
+                    )}
+                    <FavoriteBorderOutlinedIcon
+                        sx={{ color: "var(--primary-color)" }}
+                    />
+                </Typography>
+            </CardContent>
+        </Card>
+    );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,6 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
+import { fetchMovies } from "../api/services/moviesService";
 
 import MovieCard from "../components/MovieCard/MovieCard";
 import Pagination from "../components/Pagination/Pagination";
@@ -8,52 +10,48 @@ import Grid from "@mui/material/Unstable_Grid2";
 import Typography from "@mui/material/Typography";
 
 export default function Home() {
-  const [currentPage, setCurrentPage] = useState(0);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [movies, setMovies] = useState([]);
+    const [totalPages, setTotalPages] = useState(1000);
 
-  const handlePageChange = (selectedPage) => {
-    setCurrentPage(selectedPage);
-    // Perform any other actions, such as fetching data for the selected page.
-  };
+    const handlePageChange = (selectedPage) => {
+        setCurrentPage(selectedPage + 1);
+    };
 
-  return (
-    <Container fluid>
-      <Typography variant="h4" sx={{ mb: 3 }}>
-        Popular Movies
-      </Typography>
-      {/*<h1 >Popular Movies</h1>*/}
-      <Grid container spacing={2}>
-        <Grid item xs={12} sm={6} md={4} lg={2}>
-          <MovieCard />
-        </Grid>
-        <Grid item xs={12} sm={6} md={4} lg={2}>
-          <MovieCard />
-        </Grid>
-        <Grid item xs={12} sm={6} md={4} lg={2}>
-          <MovieCard />
-        </Grid>
-        <Grid item xs={12} sm={6} md={4} lg={2}>
-          <MovieCard />
-        </Grid>
-        <Grid item xs={12} sm={6} md={4} lg={2}>
-          <MovieCard />
-        </Grid>
-        <Grid item xs={12} sm={6} md={4} lg={2}>
-          <MovieCard />
-        </Grid>
-        <Grid item xs={12} sm={6} md={4} lg={2}>
-          <MovieCard />
-        </Grid>
-        <Grid item xs={12} sm={6} md={4} lg={2}>
-          <MovieCard />
-        </Grid>
-        <Grid item xs={12} sm={6} md={4} lg={2}>
-          <MovieCard />
-        </Grid>
-        <Grid item xs={12} sm={6} md={4} lg={2}>
-          <MovieCard />
-        </Grid>
-      </Grid>
-      <Pagination pageCount={10} onPageChange={handlePageChange} />
-    </Container>
-  );
+    useEffect(() => {
+        fetchMovies(currentPage)
+            .then((data) => {
+                setMovies(data.results);
+                setTotalPages(data.total_pages);
+            })
+            .catch((err) => console.log(err));
+    }, [currentPage]);
+
+    return (
+        <Container fluid="true">
+            <Typography variant="h4" sx={{ mb: 3 }}>
+                Popular Movies
+            </Typography>
+            {/*<h1 >Popular Movies</h1>*/}
+            <Grid container spacing={2}>
+                {movies &&
+                    movies.map((movie) => (
+                        <Grid
+                            item="true"
+                            xs={12}
+                            sm={6}
+                            md={4}
+                            lg={2}
+                            key={movie.id}
+                        >
+                            <MovieCard movie={movie} />
+                        </Grid>
+                    ))}
+            </Grid>
+            <Pagination
+                pageCount={totalPages}
+                onPageChange={handlePageChange}
+            />
+        </Container>
+    );
 }
