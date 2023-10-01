d00a246bfc1366767ca40f49a617861ca2f8a2e7
Aalaa <aalaabadr268@gmail.com>
2023-10-02 01:54:57 +0300

    fixed

--- src/components/WatchList/WatchListCard.jsx
+++ src/components/WatchList/WatchListCard.jsx
@@ -1,16 +1,21 @@
 import * as React from "react";
-
+import { useNavigate } from "react-router-dom";
 import { getImageUrl } from "../../api/services/imageServices";
 
 import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
 import Box from '@mui/material/Box';
-import CardMedia from '@mui/material/CardMedia';
 import Rating from '@mui/material/Rating';
 import FavoriteIcon from '@mui/icons-material/Favorite';
+import Grid from "@mui/material/Grid";
 
 export default function WatchListCard(props) {
+    const navigate = useNavigate();
+
+    const handleMovieDetails = (movieId) => {
+        navigate(`/movies/${movieId}`);
+    };
+
     return (
         <Card
             sx={{
@@ -26,85 +31,87 @@ export default function WatchListCard(props) {
                 },
             }}
         >
-            <CardMedia
-                component="img"
-                style={{ width: "30%", borderRadius: "10px", marginRight: "10px" }}
-                image={getImageUrl(props.movie.poster_path)}
-            />
-            <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-                <CardContent >
-                    <Typography
-                        variant="h3"
-                        component="div"
-                        color="var(--secondary-color)"
-                        sx={{
-                            fontWeight: "bold",
+            <Grid
+                container
+                spacing={2}
+            >
+                <Grid item xs={12} sm={6} md={4} lg={4}
+                 className="figure-hover"
+                 onClick={() => handleMovieDetails(props.movie.id)}
+                >
+                    <img
+                        src={getImageUrl(props.movie.poster_path)}
+                        style={{
+                            width: "100%",
+                            borderRadius: "10px",
+                            height: "100%",
+                        }}
+                        alt={props.movie.original_title}
+                    />
+                </Grid>
+                <Grid item xs={12} sm={6} md={4} lg={8}>
+                    <Box
+                        style={{
                             display: "flex",
                             justifyContent: "space-between",
-                            alignItems: "center",
                         }}
                     >
-                        {props.movie.original_title}
+                        <Box>
+                            <Typography
+                                variant="h3"
+                                component="h3"
+                                sx={{
+                                    fontWeight: "bold",
+                                    color: "var(--text-color)",
+                                }}
+                            >
+                                {props.movie.original_title}
+                            </Typography>
+                            <Typography variant="subtitle1">
+                                {new Date(
+                                    props.movie.release_date
+                                ).toLocaleDateString("en-US", {
+                                    month: "short",
+                                    day: "numeric",
+                                    year: "numeric",
+                                })}
+                            </Typography>
+                        </Box>
                         <FavoriteIcon
                             fontSize="large"
                             sx={{ color: "var(--primary-color)" }}
                         />
-                    </Typography>
-
-                    <Typography
-                        gutterBottom
-                        variant="subtitle1"
-                        color="var(--text-color)"
-                        sx={{
-                            fontSize: "0.874rem",
-                            display: "flex",
-                            justifyContent: "space-between",
-                            alignItems: "center",
-                        }}
-                    >
-                        {new Date(props.movie.release_date).toLocaleDateString(
-                            "en-US",
-                            {
-                                month: "short",
-                                day: "numeric",
-                                year: "numeric",
-                            }
-                        )}
-                    </Typography>
-                    
-                    <Box
-                        sx={{
-                            display: "flex",
-                            justifyContent: "flex-start",
-                            alignItems: "center",
-                        }}
-                    >
-                    <Rating
-                        name="read-only"
-                        value={props.movie.vote_average / 2}
-                        readOnly
-                        style={{ color: 'var(--text-color)' }}
-                    />
-
-                    <Typography
-                        sx={{ fontSize: "0.7rem" }}
-                    >
-                        {props.movie.vote_count}
-                    </Typography>
                     </Box>
+                    {props.movie.vote_average && (
+                        <Box sx={{
+                            display: "flex",
+                            justifyContent: "",
+                            alignItems: "center",
+                        }}>
+                            <Rating
+                                value={props.movie.vote_average / 2}
+                                precision={0.5}
+                                sx={{
+                                    mb: 2,
+                                    color: "var(--primary-color)",
+                                }}
+                                readOnly
+                            />
+                            <Typography
+                                sx={{ 
+                                    mb: 2, 
+                                    fontSize: "0.7rem" }}
+                            >
+                                {props.movie.vote_count}
+                            </Typography>
+                        </Box>
+                    )}
 
-                    <Typography
-                        // noWrap
-                        display="block"
-                        variant="subtitle2"
-                        component="div"
-                        color="var(--text-color)"
-                    >
+                    <p style={{ marginBottom: "20px" }}>
                         {props.movie.overview}
-                    </Typography>
-                </CardContent>
-
-            </Box>
+                    </p>
+                </Grid>
+            </Grid>
 
         </Card>
     );
